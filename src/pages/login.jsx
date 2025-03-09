9ea3b2f8c1f81e3e81924febcfa8b5a75fd1572b
sachinchib <sachinrajput0133@gmail.com>
2025-03-09 18:02:46 +0530

    build

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h1 className="text-center text-3xl font-extrabold text-gray-900">CollabDocs ch</h1>
+          <h1 className="text-center text-3xl font-extrabold text-gray-900">CollabDocs ch aaa</h1>
           <h2 className="mt-6 text-center text-2xl font-bold text-gray-900">
             Sign in to your account
           </h2>
